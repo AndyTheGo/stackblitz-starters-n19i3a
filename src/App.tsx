5bdb86727b02f2b1465bfede6ead6684b0995d17
AndyTheGo <43911423+AndyTheGo@users.noreply.github.com>
2023-06-10 00:41:52 -0400

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './style.css';
 
 export default function App() {
   return (
-    <div id = "body_bio">
+    <body>
+    <div className="off">
       <h1>Hello Everyone!</h1>
       <p>Welcome to my personal page!</p>
       <h3>A note from Andy:</h3>
       <h4>Hey Everyone! I hope all is well. This page has been something that I've wanted to have for quite some time now (About 2 years or so. So I hope you all enjoy, and feel free to hit me up so we can form connections! Thank you for your time!</h4>
     </div>
+    </body>
   );
 }
